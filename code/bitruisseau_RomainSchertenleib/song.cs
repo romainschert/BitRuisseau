e0bee1d8050ac2f1fdef91df4a6fcd8c531b029c
romainschert <150144520+romainschert@users.noreply.github.com>
2025-12-05 10:02:37 +0100

    task(song.cs): faire le classe Song
    [15] [Done]

---
+++ code/bitruisseau_RomainSchertenleib/song.cs
@@ -0,0 +1,21 @@
+﻿using BitRuisseau;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bitruisseau_RomainSchertenleib
+{
+    internal class song : ISong
+    {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public int Year { get; set; }
+        public TimeSpan Duration { get; set; }
+        public int Size { get; set; }
+        public string[] Featuring { get; set; }
+        public string Hash { get; set; }
+        public string Extension { get; set; }
+    }
+}
